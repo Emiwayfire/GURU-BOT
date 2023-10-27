f0e4ae99d1c46c794de9d5a0a873d4b55721ddba
Emiwayfire <116963744+Emiwayfire@users.noreply.github.com>
2023-10-27 17:45:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  [918576898666', '𓆩ꪖꪗꪖꪖꪀ ꫀꪑ𝓲𝘳࿐🪐⃤𓆪', true],
+  [918576898666', '𓆩ꪖꪗꪖꪖꪀ ꫀꪑ𝓲𝘳࿐🪐⃤𓆪', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𓆩ꪖꪗꪖꪖꪀ ꫀꪑ𝓲𝘳࿐🪐⃤𓆪'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '𓆩ꪖꪗꪖꪖꪀ ꫀꪑ𝓲𝘳࿐🪐⃤𓆪' 
+global.author = '꧁:-{ :уυנι/ѕυкυηα: }-:꧂' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
